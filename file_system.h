e9b57c7f1b0aa2c338b051c32a3439912cb1e8a0
sergxlove <sergxlove@mail.ru>
2024-02-16 22:41:06 +0600

    create a file system

--- file_system.h
+++ file_system.h
@@ -1,1 +1,14 @@
 #pragma once
+#ifndef _FILE_SYSTEM_H_
+#define _FILE_SYSTEM_H_
+#include <iostream>
+#include <fstream>
+class file_sys
+{
+public:
+	void starts();
+private:
+	void create_file();
+	bool check_file();
+};
+#endif // !_FILE_SYSTEM_H_
